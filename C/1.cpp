c488758e1255f9a4fc7dfae506cda0acb5c10af0
YOUR-SUN <YOUR-SUN@outlook.com>
2023-04-05 13:10:26 +0800

    hhh

---
+++ C/1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+	int a=1;
+	float b=2.5;
+	char c='A';
+	printf("a=%d\n",a);
+	printf("b=%f\n",b);
+	
+	return 0;
+}
---
+++ C/2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a=6,b=9;
+	double c=9.7,d=2.4;
+	printf("a*b=%d\n",a*b);
+	printf("c+d/2=%f\n",c+d/2);
+	printf("%d\n",a%b);
+	printf("(c+d)/2=%f\n",(c+d)/2);
+	return 0;
+}
