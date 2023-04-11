cd2bb298b0dc16c85861e4a6b53e5dfd48a385b8
YOUR-SUN <YOUR-SUN@outlook.com>
2023-04-11 23:10:51 +0800

    使用数组存储的斐波那契数列

--- C++/test3.cpp
+++ C++/test3.cpp
@@ -1,23 +1,38 @@
 #include<iostream>
 using namespace std;
 
-double m[] = {0.1,0.2,0.5,1.0};
-int dp[100];
-int read(int &a, int &b);
+// int f[1000];
+int *f;
+int fun(int n) {
+    if(n == 0) {
+        f[0] = 0;
+        return 0;
+    }
+    else if(n == 1 || n == 2) {
+        f[0] = 1;
+        return 1;
+    }
+    else if(f[n])   return f[n];
+    else {
+        f[n] = fun(n-1) + fun(n-2);
+        return f[n];
+    }
+}
+
 int work() {
-    int a,b;
-    int flag = read(a,b);
-    if(flag) {
-        return -1;
+    int n;
+    cin>>n;
+    f = new int[n+1];
+    for(int i = 0; i <= n; i++) {
+        f[i] = 0;
     }
-    double x = a + b*0.1;
-    //完全背包
-    for(int i = 0; i < 4; i++) {
-        for(double j = 0; j <= x; j += 0.1) {
-            dp[(int)(j*10)] = min(dp[(int)(j*10)], dp[(int)((j-m[i])*10)] + 1);
-        }
+    fun(n);
+    for(int i=1; i <= n; i++) {
+        cout << fun(i) << "  ";
+        // printf("%d ", fun(i));
     }
-    cout << dp[(int)(x*10)] << endl;
+    putchar('\n');
+    delete[]f;
     return 0;
 }
 
@@ -25,29 +40,5 @@ int main() {
     while(1) {
         work();
     }
-    return 0;
-}
-
-int read(int &a, int &b) {
-    cout << "Input: ";
-    char str[100];
-    gets(str);
-    a = b = 0;
-    char ch;
-    int i = 0;
-    while(1){
-        ch = str[i];
-        if(ch == '.' || ch == '\0')   break;
-        a = a * 10 + ch - '0';
-        i++;
-    }
-    if(ch == '\0') {
-        return 0;
-    }
-    if(str[i+2] != '\0') {
-        cout << "Invalid input: " << endl;
-        return -1;
-    }
-    b = str[i+1];
     return 0;
 }
